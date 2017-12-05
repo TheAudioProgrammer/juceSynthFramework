41c8bce91ff5e293f83fb9686a6e9eb2e6dcbeac
Joshua Hodge <joshuahodge@hotmail.com>
2017-12-05 21:00:05 +0000

    fixed midi

--- Source/SynthVoice.h
+++ Source/SynthVoice.h
@@ -12,6 +12,7 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "SynthSound.h"
+#include "Maximilian.h"
 
 
 class SynthVoice : public SynthesiserVoice
@@ -28,22 +29,49 @@ public:
     
     void startNote (int midiNoteNumber, float velocity, SynthesiserSound* sound, int currentPitchWheelPosition)
     {
+        env1.trigger = 1;
         frequency = MidiMessage::getMidiNoteInHertz(midiNoteNumber);
-        std::cout << midiNoteNumber << std::endl;
+        level = velocity;
     }
     
     //=======================================================
     
     void stopNote (float velocity, bool allowTailOff)
     {
-        clearCurrentNote();
+        env1.trigger = 0;
+        
+        allowTailOff = true;
+        
+        if (velocity == 0)
+            clearCurrentNote();
+        
     }
     
     //=======================================================
     
     void renderNextBlock (AudioBuffer <float> &outputBuffer, int startSample, int numSamples)
     {
+        env1.setAttack(1000);
+        env1.setDecay(500);
+        env1.setSustain(0.8);
+        env1.setRelease(2000);
+
         
+        
+        for (int sample = 0; sample < numSamples; ++sample)
+        {
+            double theWave = osc1.sinewave(frequency);
+            double theSound = env1.adsr(theWave, env1.trigger) * level;
+            
+            
+            for (int channel = 0; channel < outputBuffer.getNumChannels(); ++channel)
+            {
+                outputBuffer.addSample(channel, startSample, theSound);
+            }
+            
+            ++startSample;
+            
+        }
         
        
     }
@@ -69,6 +97,9 @@ public:
 private:
     double level;
     double frequency;
+    
+    maxiOsc osc1;
+    maxiEnv env1;
     
     
     
